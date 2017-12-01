6561c92df3a5dee1e7c080f01dce6ae5742ea865
Paul Bitutsky <pbitutsky@berkeley.edu>
2017-11-30 22:32:00 -0800

    fixed random

--- generate_output_files.py
+++ generate_output_files.py
@@ -1,7 +1,31 @@
-import solver
+import Solver
 import output_validator
+import randomAlt
 
 def generate_output_files(num_wizards):
+<<<<<<< HEAD
+    for i in range(10):
+        infile = 'phase2_inputs/inputs{0}/input{0}_{1}.in'.format(num_wizards, i)
+        outfile = 'outputs/output{0}_{1}.out'.format(num_wizards, i)
+        # try:
+        Solver.main(infile, outfile)
+        output_validator.main([infile, outfile])
+    # except:
+    # 	print("error")
+
+
+# generate_output_files(35)
+
+def random_solve(num_wizards, i):
+    infile = 'phase2_inputs/inputs{0}/input{0}_{1}.in'.format(num_wizards, i)
+    outfile = 'outputs/output{0}_{1}.out'.format(num_wizards, i)
+    # try:
+    num_wizards, num_constraints, wizards, constraints = Solver.read_input(infile)
+    randomAlt.random_check(constraints, wizards)
+    output_validator.main([infile, outfile])
+
+random_solve(50, 9)
+=======
 	for i in range(10):
 		infile = 'phase2_inputs/inputs{0}/input{0}_{1}.in'.format(num_wizards, i)
 		outfile = 'outputs/output{0}_{1}.out'.format(num_wizards, i)
@@ -12,3 +36,4 @@ def generate_output_files(num_wizards):
 		# 	print("error")
 		
 generate_output_files(50)
+>>>>>>> a41effa322c0124f2fcb4f57c88649aba6bd4c82
--- randomAlt.py
+++ randomAlt.py
@@ -1,5 +1,23 @@
 import random
 import util
+<<<<<<< HEAD
+import Solver
+
+import output_validator
+
+def random_check(constraints, start_ordering):
+    print("STARTING")
+    ordering = start_ordering
+    maximum = float('-inf')
+    constraints_satisfied = 0
+    while constraints_satisfied != len(constraints):
+        random.shuffle(ordering)
+        if constraints_satisfied > maximum:
+            maximum = constraints_satisfied
+            max_violations = ordering
+            print("New Max: ", maximum, max_violations)
+        constraints_satisfied = validate_constraints(constraints, ordering)
+=======
 import OldSolver
 def random_check(constraints, start_ordering):
     print("HAPPENING");
@@ -25,9 +43,28 @@ def random_check(constraints, start_ordering):
             print(len(violations))
             return ordering
     print('violations: ' + violations)
+>>>>>>> a41effa322c0124f2fcb4f57c88649aba6bd4c82
     return ordering
 
+def validate_constraints(constraints, output_ordering):
+    constraints_satisfied = 0
+    constraints_failed = []
+    for constraint in constraints:
+        c = constraint  # Creating an alias for easy reference
+        m = {k: v for v, k in enumerate(output_ordering)}  # Creating an alias for easy reference
+
+        wiz_a = m[c[0]]
+        wiz_b = m[c[1]]
+        wiz_mid = m[c[2]]
+
+        if (wiz_a < wiz_mid < wiz_b) or (wiz_b < wiz_mid < wiz_a):
+            constraints_failed.append(c)
+        else:
+            constraints_satisfied += 1
+
+<<<<<<< HEAD
+    return constraints_satisfied
+=======
 
 
-
-
+>>>>>>> a41effa322c0124f2fcb4f57c88649aba6bd4c82
