afce0366b500a712235dd1be8f882a14f240a8ca
EKATERINA BROOUN <katyabrooun@berkeley.edu>
2017-12-01 00:50:30 -0800

    added infrastricture for testing

--- hail_mary.py
+++ hail_mary.py
@@ -1,41 +1,71 @@
 
+import argparse
+
 variable_map = {}
 SAT_clauses = []
 
 # PAUL
 def generate_variables(wizards):
-	pass
+    pass
 
 # PAUL
 def generate_constraint_clauses(constraints):
-	pass
+    pass
 
 # KATYA
 def generate_transitivity_clauses(wizards):
-	pass
+    pass
 
-class GraphNode:
+# class GraphNode:
 
 
 def generate_var_relationship_graph(true_variables):
-	pass
+    pass
 
 # input is the SAT clauses, output is true_variables
 def solve_SAT():
-	pass
+    pass
 
 # run topological sort on graph, returns wizard ordering 
 def find_ordering(graph):
-	pass
+    pass
 
 def solve(wizards, constraints):
-	generate_variables(wizards)
-	generate_constraint_clauses(constraints)
-	generate_transitivity_clauses(wizards)
-	
-	true_variables = solve_SAT(SAT_clauses)
-	graph = generate_var_relationship_graph(true_variables)
-	result = find_ordering(graph)
-	return result
+    generate_variables(wizards)
+    generate_constraint_clauses(constraints)
+    generate_transitivity_clauses(wizards)
+    
+    true_variables = solve_SAT(SAT_clauses)
+    graph = generate_var_relationship_graph(true_variables)
+    result = find_ordering(graph)
+    return result
+
+def read_input(filename):
+    with open(filename) as f:
+        num_wizards = int(f.readline())
+        num_constraints = int(f.readline())
+        constraints = []
+        wizards = set()
+        for _ in range(num_constraints):
+            c = f.readline().split()
+            constraints.append(c)
+            for w in c:
+                wizards.add(w)
+                
+    wizards = list(wizards)
+    return num_wizards, num_constraints, wizards, constraints
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description = "Constraint Solver.")
+    parser.add_argument("input_file", type=str, help = "___.in")
+    args = parser.parse_args()
+    num_wizards, num_constraints, wizards, constraints = read_input(args.input_file)
+
+    # TEST HERE
+    print(wizards)
+
+
+
+
 
 
